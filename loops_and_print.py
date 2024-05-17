b58b54d75ace7b72de35477cb2aa01b3dffe8966
mayraaoteo <162721507+mayraaoteo@users.noreply.github.com>
2024-05-17 12:20:00 -0300

    Entrego loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,23 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(my_list):
+    new_list = []
+    i = 0
+    for element in my_list:
+        if element != "":
+            new_elem = f"{i}. {element}"
+            new_list.append(new_elem)
+            i += 1
+
+    return new_list
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(my_list):
+    new_list = []
+    i = 0
+    for element in my_list:
+        if element != "":
+            word = element[:: -1]
+            new_elem = f"{i}. {word}"
+            new_list.append(new_elem)
+            i += 1
+
+    return new_list
